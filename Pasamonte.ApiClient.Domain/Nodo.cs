e40701fafd78a3b011328bc9477cb5d595f7d39b
pasamonte <cristian.ormazabal@pasamonte.cl>
2014-07-19 22:17:14 -0400

    Faltantes de domain

---
+++ Pasamonte.ApiClient.Domain/Nodo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pasamonte.ApiClient.Domain
+{
+    /// <summary>
+    /// Representa un nodo de la red
+    /// </summary>
+    [DataContract(IsReference = true)]
+    public class Nodo : AuditInfoComplete
+    {
+        /// <summary>
+        /// Post identity
+        /// </summary>
+        [Key]
+        [DataMember]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Identificador del tipo de nodo
+        /// </summary>
+        [DataMember]
+        public Guid IdTipoNodo { get; set; }
+
+        /// <summary>
+        /// TipoNodo
+        /// </summary>
+        [ForeignKey("IdTipoNodo")]
+        [DataMember]
+        public TipoNodo TipoNodo { get; set; }
+
+        /// <summary>
+        /// Titulo
+        /// </summary>
+        [Required]
+        [StringLength(200)]
+        [DataMember]
+        public string Titulo { get; set; }
+
+        /// <summary>
+        /// Descripcion for this article
+        /// </summary>
+        [Required]
+        [StringLength(500)]
+        [DataMember]
+        public string Descripcion { get; set; }
+
+        /// <summary>
+        /// Url for the image representing the post
+        /// </summary>
+        [Required]
+        [StringLength(500)]
+        [DataType(DataType.ImageUrl)]
+        [DataMember]
+        public string UrlImagen { get; set; }
+
+        /// <summary>
+        /// If the article is published and visible to others or not
+        /// </summary>
+        [DataMember]
+        [Required]
+        public bool Publico { get; set; }
+
+        /// <summary>
+        /// Related Tags
+        /// </summary>
+        [DataMember]
+        public ICollection<NodoTag> NodoTags { get; set; }
+
+        /// <summary>
+        /// IdNodoPadre
+        /// </summary>
+        [DataMember]
+        public Guid? IdNodoPadre { get; set; }
+
+        /// <summary>
+        /// NodoPadre
+        /// </summary>
+        [ForeignKey("IdNodoPadre")]
+        [DataMember]
+        public Nodo NodoPadre { get; set; }
+
+        /// <summary>
+        /// CodigoPublico - codigo oficial del nodo
+        /// </summary>
+        [DataMember]
+        [MaxLength(256)]
+        public string CodigoPublico { get; set; }
+    }
+
+    /// <summary>
+    /// Tags for the Nodos
+    /// </summary>
+    [DataContract(IsReference = true)]
+    public class NodoTag : BaseTag<Nodo>
+    {
+    }
+}
---
+++ Pasamonte.ApiClient.Foo/Program.cs
@@ -0,0 +1,74 @@
+﻿using Pasamonte.ApiClient.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pasamonte.ApiClient.Foo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var url = "http://54.191.151.49";
+            var url = "http://localhost:22658/";
+            var query = string.Empty;
+            //query = @"$filter=Id eq (guid'08bb5553-6747-45d1-a2bb-db5872c4c3fb')";
+            var apiClient = new ApiClient();
+            var resultado = apiClient.PslGetNodos(
+                url,
+                null,
+                query
+            ).Result;
+            Console.ReadKey();
+            //var identificacionUsuario = new IdentificacionUsuario()
+            //{
+            //    Canal = Core.CanalIdentificacion.Teclado,
+            //    Clave = "123456",
+            //    Identificador = "141377523",
+            //    Tipo = Core.TipoIdentificacion.Cedula
+            //};
+            //var identificacionTerminal = new IdentificacionTerminal()
+            //{
+            //    Codigo = "ac151216-1be5-4073-99a6-a3f8a359fe4f",
+            //    CodigoTipoTerminal = Core.CodigoTipoTerminal.DispensadorFarmacos
+            //};
+            //var identificacionSistemaRemoto = new IdentificacionSistemaRemoto()
+            //{
+            //    Codigo = "RAYEN"
+            //};
+            //var taskAutenticar = apiClient.RceAutenticar
+            //    (
+            //        "http://localhost:22658/",
+            //        null,
+            //        identificacionUsuario,
+            //        identificacionTerminal,
+            //        identificacionSistemaRemoto
+            //    );
+            //taskAutenticar.Wait();
+            //var taskObtenerEntregas = apiClient.RceObtenerEntregas
+            //    (
+            //        "http://localhost:22658/",
+            //        null,
+            //        identificacionUsuario,
+            //        identificacionTerminal,
+            //        identificacionSistemaRemoto
+            //    );
+            //taskObtenerEntregas.Wait();
+            //var task = apiClient.RceNotificarEntrega
+            //    (
+            //        "http://localhost:22658/",
+            //        null,
+            //        identificacionUsuario,
+            //        identificacionTerminal,
+            //        identificacionSistemaRemoto,
+            //        new Entrega()
+            //        {
+            //            IdEnSistemaExterno = "123456"
+            //        }
+            //    );
+           Console.ReadKey();
+        }
+    }
+}
