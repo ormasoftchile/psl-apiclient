a22c7b098aa591aaabfa32888d838c5ab9a2bd20
pasamonte <cristian.ormazabal@pasamonte.cl>
2015-06-08 09:59:44 -0400

    Creacion de paquetes nuget

--- Pasamonte.ApiClient.Core.Nuget/Properties/AssemblyInfo.cs
+++ Pasamonte.ApiClient.Core.Nuget/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Pasamonte.ApiClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Pasamonte.ApiClient.Core.Nuget")]
+[assembly: AssemblyDescription("Project template made by EyeCatch (http://www.eyecatch.no/)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Pasamonte.ApiClient")]
+[assembly: AssemblyProduct("Pasamonte.ApiClient.Core.Nuget")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("546fc755-81ae-4a21-8b7a-940546c15e3a")]
+[assembly: Guid("5b586874-8d3e-4137-9ba8-e982413416d5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.9")]
-[assembly: AssemblyFileVersion("1.2.0.9")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
